a8598edda8f1bd2b576c1d0f6493cac4ba58e6ec
bnanaday <bnanaday@gmail.com>
2022-04-01 23:23:44 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-vjaqd5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-vjaqd5)
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,24 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 07, name: 'Flexy' },
+  { id: 08, name: 'RockWoman' },
+  { id: 09, name: 'Hulk' },
+  { id: 10, name: 'RobotMan' },
+  { id: 11, name: 'Dr Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' },
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
